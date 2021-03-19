8cf3dd14e613ed381e419b77ebe142b1695d5c0e
Michael Raysor <michaelraysor1@gmail.com>
2021-03-19 01:31:27 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,12 @@
 def using_while
   levitation_force = 6
-	
-  #your code here
 end
+
+	while loop levitation_force <= 9
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+    end
+  #your code here
+
 
 

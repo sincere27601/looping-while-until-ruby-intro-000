161174cc93380ea9db2827c9f86719e361bedf94
Michael Raysor <michaelraysor1@gmail.com>
2021-03-19 01:35:48 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,10 @@
 def using_until
         levitation_force = 6
       
-
-until levitation_force == 10
+until levitation_force == 10 do
+       puts "Wingardium Leviosa"
         levitation_force += 1
-        puts "Wingardium Leviosa"
+      
 
       end
+    end
